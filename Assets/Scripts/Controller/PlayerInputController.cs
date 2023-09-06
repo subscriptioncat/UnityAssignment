4f887dfedf99f74649ec51ec4458b01b97e9873f
KimDoHyeon <hj2471755@naver.com>
2023-09-06 23:46:16 +0900

    기본

---
+++ Assets/Scripts/Controller/PlayerInputController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : TopDownCharacterController
+{
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveInput);
+    }
+    public void OnLook(InputValue value)
+    {
+        Vector2 newAim = value.Get<Vector2>();
+        Vector2 WorldPos = _camera.ScreenToWorldPoint(newAim);
+        newAim = (WorldPos - (Vector2)transform.position).normalized;
+
+        if (newAim.magnitude >= .9f)
+        {
+            CallLookEvent(newAim);
+        }
+    }
+    public void OnFire(InputValue value)
+    {
+        IsAttacking = value.isPressed;
+    }
+}
---
+++ Assets/Scripts/Controller/TopDownCharacterController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
+using UnityEngine;
+
+public class TopDownCharacterController : MonoBehaviour
+{
+    public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnLookEvent;
+    public event Action OnAttackEvent;
+
+    private float _timeSinceLastAttack = float.MaxValue;
+    protected bool IsAttacking { get; set; }
+
+    protected virtual void Update()
+    {
+        HandleAttackDelay();
+    }
+    private void HandleAttackDelay()
+    {
+        if (_timeSinceLastAttack <= 0.2f)
+        {
+            _timeSinceLastAttack += Time.deltaTime;
+        }
+        if (IsAttacking && _timeSinceLastAttack > 0.2f)
+        {
+            _timeSinceLastAttack = 0;
+            CallAttackEvent();
+        }
+    }
+
+    public void CallMoveEvent(Vector2 direction)
+    {
+        OnMoveEvent?.Invoke(direction);
+    }
+    public void CallLookEvent(Vector2 direction)
+    {
+        OnLookEvent?.Invoke(direction);
+    }
+    public void CallAttackEvent()
+    {
+        OnAttackEvent?.Invoke();
+    }
+}
